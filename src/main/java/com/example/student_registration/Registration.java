61714230af193771cd1496e1c1cb23429d6656c3
dirtyphonicsss@yandex.ru <dirtyphonicsss@yandex.ru>
2024-04-30 16:37:01 +0300

    first one

---
+++ src/main/java/com/example/student_registration/Registration.java
@@ -0,0 +1,83 @@
+package com.example.student_registration;
+
+import com.example.student_registration.model.Student;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@ShellComponent
+public class Registration {
+    private final Map<Integer, Student> students = new HashMap<>();
+
+    @ShellMethod(key = "i")
+    public String init(@ShellOption(value = "n") String name,
+                       @ShellOption(value = "l") String lastName,
+                       @ShellOption(value = "a") Integer age) {
+//        System.out.println("Введите данные студента в формате: init --n Name --l Second name --a age");
+        Student student = new Student();
+        student.setName(name);
+        student.setLastName(lastName);
+        student.setAge(age);
+        Integer id = idGenerator();
+        students.put(id, student);
+        return MessageFormat.format("Студент: {0} {1} {2}, id №{3}  записан!",
+                student.getName(), student.getLastName(), student.getAge(), id);
+
+    }
+
+
+    private Integer idGenerator() {
+        Integer id = 0;
+        if (students.isEmpty()) {
+            id = 1;
+        } else {
+            id = Collections.max(students.keySet()) + 1;
+        }
+        return id;
+    }
+
+    @ShellMethod(key = "sh")
+    public void showAllStudents() {
+        if (students.isEmpty()){
+            System.out.println("Список студентов пустой");
+        } else {
+            students.entrySet().forEach(entry -> {
+                System.out.println(entry.getValue().toString() + ", id №" + entry.getKey());
+            });
+        }
+    }
+
+    @ShellMethod(key = "d")
+    public void deleteStudent(@ShellOption(value = "id") int id){
+        if(!students.containsKey(id)){
+            System.out.printf("Студент с id №%s отсутствует в списке!\n", id);
+        } else{
+            students.remove(id);
+            System.out.format("Студент с id №%s удален!\n", id);
+        }
+    }
+
+    @ShellMethod(key = "c")
+    public void clear(){
+        if(students.isEmpty()){
+            System.out.println("Список студентов пуст!\n");
+        } else{
+            students.clear();
+            System.out.println("Список студентов очищен!\n");
+        }
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void crateStudent(){
+        System.out.println("Создайте студента. Введите имя, фамилия, возраст: ");
+//        init(name, lastName, age);
+    }
+
+}
---
+++ src/main/java/com/example/student_registration/model/Student.java
@@ -0,0 +1,10 @@
+package com.example.student_registration.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String name;
+    private String lastName;
+    private int age;
+}
